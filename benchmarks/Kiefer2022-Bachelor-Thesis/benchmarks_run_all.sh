d0b8d8d5e30259be2c7a40ccc935e21cbe409b66
Mario Kiefer <m.kiefer@lakdev.de>
2022-10-29 15:15:25 +0200

    Add run all benchmark script

---
+++ benchmarks/Kiefer2022-Bachelor-Thesis/benchmarks_run_all.sh
@@ -0,0 +1,8 @@
+bash benchmark_fig1_cpp_merged.sh
+bash benchmark_fig1_cpp_sep.sh
+bash benchmark_fig1_cuda_merged.sh
+bash benchmark_fig1_cuda_sep.sh
+bash benchmark_fig2_cpp_merged_profiling.sh
+bash benchmark_fig2_cpp_sep_profiling.sh
+bash benchmark_fig2_cuda_merged_profiling.sh
+bash benchmark_fig2_cuda_sep_profiling.sh
